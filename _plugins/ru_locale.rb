075f44968023bd809ef03f82e6274ae13bfc62ec
klimov-rv <klimov.rv@ya.ru>
2017-05-14 22:27:45 +0300

    New logo

--- _plugins/ru_locale.rb
+++
@@ -1,9 +0,0 @@
-module DateFilter
-  MONTHS = %w(Января Февраля Марта Апреля Марта Мая Июня Августа Сентября Октября Ноября Декабря)
-
-  def russian_long_month(input)
-    input.strftime("%d") + " " + MONTHS[input.strftime("%m").to_i - 1] + " " + input.strftime("%Y")
-  end
-end
-
-Liquid::Template.register_filter(DateFilter)
