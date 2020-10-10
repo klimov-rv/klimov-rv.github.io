aae02a618d47a60052da7c0f5165080c7fac7b77
klimov-rv <ms@fsdf.ret>
2020-10-10 21:43:32 +0300

    Nuxt JS - Vue + SSR
    https://www.youtube.com/watch?v=lm9olMCRCIc

---
+++ projects/nuxt-crash-course/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+      <h1>About page</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto velit quod nihil, quaerat tempora assumenda nobis impedit natus quam nam ab autem deleniti explicabo eius placeat repellendus dolores laboriosam repudiandae ratione dicta quos minus culpa vel porro! Porro laboriosam explicabo quibusdam, nobis unde et tempora inventore impedit harum, hic pariatur voluptatibus.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, soluta. Labore delectus quam vitae quaerat?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto velit quod nihil, quaerat tempora assumenda nobis impedit natus quam nam ab autem deleniti explicabo eius placeat repellendus dolores laboriosam repudiandae ratione dicta quos minus culpa vel porro! Porro laboriosam explicabo quibusdam, nobis unde et tempora inventore impedit harum, hic pariatur voluptatibus.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
+
+<style>
+
+</style>
---
+++ projects/nuxt-crash-course/pages/users/_id.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <h1>User {{ user.name }} </h1>
+        <hr>
+        <h3>
+            {{ user.email }} 
+        </h3>
+        <p>{{ user.phone }}, {{ user.username }}</p>
+    </section>
+</template>
+<script>
+export default {
+    validate({params}) {
+        return /^\d+$/.test(params.id)
+    },
+    async asyncData({$axios, params}) {
+        const user = await $axios.$get('http://jsonplaceholder.typicode.com/users/' + params.id)
+        return {user}
+    }
+}
+</script>
