fd619335bb7441ba12dd0b886bd6186e75fc5778
klimov-rv <ms@fsdf.ret>
2020-10-15 19:18:03 +0300

    jekyll updated local

---
+++ _site/projects/nuxt-crash-course/layouts/empty.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="empty-layout">
+        <nuxt />
+    </div>
+</template>
+
+<style scoped>
+    .empty-layout {
+        display: flex;
+        justify-content: center;
+        padding: 5rem;
+        align-items: center;
+    }
+</style>
