75150da74776cd1a77dd13ef141a2e58b4e6cbe5
klimov-rv <klimov.rv@yandex.ru>
2026-01-09 23:04:24 +0300

    new site

--- _drafts/sass.md
+++
@@ -1,1 +0,0 @@
-http://www.internet-technologies.ru/articles/izuchaem-sass-rukovodstvo-dlya-novichka.html
--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(".grid-item").hover(function(){
-    $(this).find( ".glass" ).stop(true, false).addClass("up");
-    $(this).find( ".glass" ).stop(true, false).removeClass("down");
-}, function() {
-    $(this).find( ".glass" ).stop(true, false).removeClass("up");
-    $(this).find( ".glass" ).stop(true, false).addClass("down");
-});
