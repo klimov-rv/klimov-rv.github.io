819a0117c3c64ef7a2562dcf063c40196dc4b5e7
klimov-rv <klimov.rv@yandex.ru>
2026-04-06 14:16:12 +0300

    build; minor updates

---
+++ _nuxt/CiD18o0t.js
@@ -0,0 +1,1 @@
+import{_ as r,o as s,c as o,v as t}from"./CK4dMXTU.js";const n={};function c(e,a){return s(),o("tr",null,[t(e.$slots,"default")])}const l=Object.assign(r(n,[["render",c]]),{__name:"ProseTr"});export{l as default};
